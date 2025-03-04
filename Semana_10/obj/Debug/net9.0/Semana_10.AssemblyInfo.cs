da76c981be8f6cf75e8d5698f1695101c6ae8189
https://github.com/DavidUEA/Tareagym.git <dh.changoa@uea.edu.ec>
2025-03-04 14:48:00 -0500

    Semana_11

--- Semana_10/obj/Debug/net9.0/Semana_10.AssemblyInfo.cs
+++ Semana_10/obj/Debug/net9.0/Semana_10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana_10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c917afbe5ec01d79cba22c3dc94211bd80c18e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df25733f9ae3ee5543c3d31f5714c77b6884234")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana_10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana_10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
